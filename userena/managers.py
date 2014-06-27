9ab09ed71e88989e8e3898d35ec9a4f32b59225a
ampelmann <thomas@sem.io>
2014-06-27 17:28:35 +0200

    fix broken import on managers.py

--- userena/managers.py
+++ userena/managers.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import Q
-from userena.compat import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 from django.contrib.auth.models import UserManager, Permission, AnonymousUser
 from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext as _
